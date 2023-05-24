81fd1a7ae092f187b451086f972cebf3908aa4e3
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-24 05:49:29 -0700

    ll

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double taxes = 0;
+        if (salary <= 10000) {taxes = salary * 0.15;}
+        else if (salary <= 20000) {taxes = salary * 0.18;}
+        else {taxes = salary * 0.20;}
 
+        double salaryAfterTaxes = salary - taxes;
+        System.out.println( salaryAfterTaxes);
         }
     }
 
