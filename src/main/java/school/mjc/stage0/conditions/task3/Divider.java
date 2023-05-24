538399e6e68a5c0f972b5a4b935d2ce1782772f4
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-24 13:34:57 -0700

    galochka

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,12 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
-        if (number % 5 == 0 && number % 11 == 0) {
-            System.out.println("Dividable");
-        } else if (number == 0) {
+
+        boolean odi = number == 0;
+        if (odi) {
             System.out.println("cannot divide by zero");
         } else {
-            System.out.println("Non-dividable");
+            boolean b = number % 5 == 0;
+            boolean c = number % 11 == 0;
+            if (b && c) {
+                System.out.println("Dividable");
+            }else {
+                System.out.println("Non-dividable");
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        int number = 32;
+        System.out.println( number % 5 == 0 && number % 11 == 0);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,13 +2,28 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        double taxes = 0;
-        if (salary <= 10000) {taxes = salary * 0.15;}
-        else if (salary <= 20000) {taxes = salary * 0.18;}
-        else {taxes = salary * 0.20;}
+        double tax;
 
-        double salaryAfterTaxes = salary - taxes;
-        System.out.println( salaryAfterTaxes);
+        if ( salary > 0 && salary   <= 10000) {
+            tax = salary * 0.15;
+
+            System.out.println(salary - tax);
+        } else if (salary > 10000 && salary <= 20000) {
+            tax = salary * 0.18;
+            System.out.println(salary - tax);
+        } else if ( salary > 20000){
+            tax = salary * 0.2;
+            System.out.println(salary - tax);
+           }else {
+        System.out.println("wrong input!");
+
         }
     }
+
+
+    }
+
+
+
+
 
